5a17aecefd114f796d887c18f0275e24205ad3fe
lherzel <lydia.herzel@outlook.com>
2016-03-22 16:53:20 -0400

    added read processing & mapping script

---
+++ shell_scripts/3PE_seq/read_processing_mapping.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+~/programs/cutadapt-1.8/bin/cutadapt -g GATTGATGGTGCCTACAG -n 1 -e 0.1 -m 15 -M 69 -o SC_R1.fastq -p SC_R2.fastq Sc_2b_Lig_014_AGTTCC_L002_R1.fastq Sc_2b_Lig_014_AGTTCC_L002_R2.fastq
+
+~/bin/fastx_trimmer -Q 33 -f 6 -i SC_R1.fastq -o SC_R1_tr.fastq
+
+R1="SC_R1_tr.fastq"
+R2="SC_R2.fastq"
+
+sh /home2/lh465/scripts/tophat2_Scer3_PE.sh $R1 $R2
